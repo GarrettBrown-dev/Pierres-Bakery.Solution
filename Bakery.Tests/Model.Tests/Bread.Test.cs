df7e7a1432ccd26e43bb5abc1cba66b6172ba5a0
Garrett <gman9mm@live.com>
2020-12-11 10:05:37 -0800

    Create good-fail test for A single bread loaf.

--- Bakery.Tests/Model.Tests/Bread.Test.cs
+++ Bakery.Tests/Model.Tests/Bread.Test.cs
@@ -13,5 +13,11 @@ namespace Bakery.Tests
             Bread newBread = new Bread(0, 0);
             Assert.AreEqual(typeof(Bread), newBread.GetType());
         }
+
+        [TestMethod]
+        public void BreadPrice_ShowPriceForSingleLoaf_5()
+        {
+            Assert.AreEqual(5, Bread.BreadPrice(1));
+        }
     }
 }
