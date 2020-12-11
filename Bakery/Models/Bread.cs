5037b33544c99d4ed7fce4b82cc65798ffa91b39
Garrett <gman9mm@live.com>
2020-12-11 09:29:18 -0800

    Create Bread and Pastry classes.

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -4,6 +4,13 @@ namespace Bakery.Models
 {
     public class Bread
     {
+        public int Price { get; set; }
+        public int Quant { get; set; }
+        public Bread(int price, int quant)
+        {
+            Price = price;
+            Quant = quant;
+        }
 
     }
 }
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,12 +1,20 @@
 using System;
+using Bakery.Models;
 
 namespace Pierre
 {
     public class Program
     {
-        public static void Main();
-        
+        public static void Main()
+        {
+            Bread bread = new Bread(0, 0);
+            Pastry pastry = new Pastry(0, 0);
 
-        
+            Console.WriteLine("Bonjour! Welcome to Pierre's Bakery! What would you like today mon ami? Can I start you off with some bread? (Please enter yes or no!)");
+            string userInputFirst = Console.ReadLine().ToLower();
+        }
     }
+
+
+
 }
